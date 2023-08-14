51dcf79a3514c01dea542214fc2efbaee9c195fe
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-08-14 17:28:45 +0300

    update code according to comments

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,22 +1,8 @@
 const app = require('../app.js');
 const request = require('supertest');
-const db = require("../db/connection");
-const seed = require("../db/seeds/seed");
-const data = require("../db/data/test-data/index");
-const format = require("pg-format");
 
-afterAll(() => {
-  return db.end();
-});
-
-beforeEach(() => {
-  return seed(data);
-});
 describe('app', () => {
   describe("api/topics", () => {
-    test("Status 200: with a message on the body", () => {
-      return request(app).get("/api/topics").expect(200);
-    });
     test("200: responds with an array of topic object", () => {
       return request(app).get("/api/topics").expect(200).then((response) => {
        const { topics } = response.body;
@@ -27,6 +13,16 @@ describe('app', () => {
        })
       })
     })
+  });
+  test("200: responds with an array of exactly 3 topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        const { topics } = response.body;
+        expect(Array.isArray(topics)).toBe(true);
+        expect(topics.length).toBe(3);
+      });
   });
 })
 
