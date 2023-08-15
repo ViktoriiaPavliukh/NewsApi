21b4961865a4cdb42c01e1cf5d45ea6858d600b6
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-08-15 15:35:53 +0300

    create /api endpoint

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
 const app = express();
 const { getTopic } = require("./controllers/topic.controllers");
+const { getEndpoints } = require("./controllers/endpoints.controllers");
+
+app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopic);
-
 
 module.exports = app;
---
+++ controllers/endpoints.controllers.js
@@ -0,0 +1,11 @@
+const endpoints = require("../endpoints.json");
+
+exports.getEndpoints = (req, res) => {
+    res.status(200).send({
+      endpoints
+  });
+};
+
+
+
+
